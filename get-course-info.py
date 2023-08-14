ff86ec906d1a424ad9bc94e584547f68f353a671
Noreh AD <akinoreh@gmail.com>
2023-08-14 14:04:59 +0300

    build output str

--- get-course-info.py
+++ get-course-info.py
@@ -2,21 +2,23 @@ from pprint import pprint
 from bs4 import BeautifulSoup
 import requests
 from helpers.general import auto_lookup
-from helpers.course import course_fields_lookup_table, get_contents
+from helpers.course import course_fields_lookup_table, get_contents, build_output_str
 
 course_url = "https://www.linkedin.com/learning/tailwind-css-essential-training"
-
 request = requests.get(course_url)
+html = request.text
 
-soup = BeautifulSoup(request.text, features="html.parser")
+soup = BeautifulSoup(html, features="html.parser")
 
 course_info = auto_lookup(course_fields_lookup_table, soup)
 get_contents(soup, course_info)
 
-course_info["Contents"] = get_contents(soup)
+course_output_str = build_output_str(course_info)
 
 
-pprint(course_info, sort_dicts=False)
+# pprint(course_info, sort_dicts=False)
+
+print(course_output_str)
 
 
 input()
--- helpers/course.py
+++ helpers/course.py
@@ -54,3 +54,78 @@ def get_contents(soup, obj):
     obj["Contents"] = chapters
     return chapters
 
+
+def build_output_str(course):
+    tab = "    "
+    
+    top_fields = ["Title", "Instructor", "Released", "Duration", "Level"]
+    longest_field = max(top_fields, key=len)
+    longest_field_output_len = len(longest_field) + (4 - len(longest_field) % 4)
+    
+    longest_video_name = ""
+    for chapter in course["Contents"]:
+        if len(chapter["Title"]) > len(longest_video_name):
+            longest_video_name = chapter["Title"]
+        for video in chapter["Videos"]:
+            if len(video["Title"]) > len(longest_video_name):
+                longest_video_name = video["Title"]
+    longest_video_name_output_len = len(longest_video_name)
+    longest_video_name_tab_offset = (4 - len(longest_video_name) % 4)
+    if longest_video_name_tab_offset <= 1:
+        longest_video_name_tab_offset = 4;
+    longest_video_name_output_len += longest_video_name_tab_offset
+    
+    output_lines = []
+    
+    for field in top_fields:
+        value = course[field]
+        line = field.ljust(longest_field_output_len, " ") + ": " + value
+        output_lines.append(line)
+    
+    output_lines.append("")
+    output_lines.append("Description")
+    output_lines.append("")
+    output_lines.append(course["Description"])
+    
+    output_lines.append("")
+    output_lines.append("Skills covered")
+    for skill in course["Skills covered"]:
+        output_lines.append(tab + skill)
+    
+    output_lines.append("")
+    output_lines.append("Contents")
+    output_lines.append(tab)
+    
+    is_contents_double_digit = len(course["Contents"]) - 1 > 9;
+    
+    for index, chapter in enumerate(course["Contents"]):
+        chapter_title_parts = chapter["Title"].split(". ")
+        chapter_title_parts = chapter_title_parts + ([None] * (2 - len(chapter_title_parts)))
+        chapter_order, chapter_title = chapter_title_parts
+        if chapter_order and chapter_title is None:
+            chapter_title = chapter_order
+            chapter_order = None
+        if chapter_title in ["Introduction", "Welcome"]:
+            chapter_order = "0"
+        elif chapter_title == "Conclusion":
+            chapter_order = str(index)
+        if is_contents_double_digit:
+            chapter_order = chapter_order.rjust(2, "0")
+        chapter_title = chapter_order + ". " + chapter_title
+        chapter_title = chapter_title.ljust(longest_video_name_output_len, " ")
+        chapter_line = tab + chapter_title + tab + chapter["Duration"]
+        output_lines.append(chapter_line)
+        
+        for video in chapter["Videos"]:
+            video_title = video["Title"]
+            video_title = video_title.ljust(longest_video_name_output_len)
+            video_line = tab + tab + video_title + video["Duration"]
+            output_lines.append(video_line)
+        
+        output_lines.append(tab)
+    
+    output = "\n".join(output_lines)
+    
+    return output
+
+
